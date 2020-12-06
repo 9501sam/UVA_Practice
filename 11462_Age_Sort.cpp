c6bf9b6cb571fda96eaa116e8eba8e3321dbabbb
9501sam <44914572+9501sam@users.noreply.github.com>
2020-12-06 18:50:14 +0800

    Add files via upload

--- 11462_Age_Sort.cpp
+++ 11462_Age_Sort.cpp
@@ -5,22 +5,34 @@ int main(){
 	int n;
 	while(cin >> n && n != 0){
 		//int a[n];
-		int queue[10][n];
-		int usedSize[10] = {};
+		int queue[10][n+3];
+		int usedSize[10] = {0};
 		int num;
-		for(int i = 0; i < n; i++){
+		for(int i = 0; i < n; i++){ //87 % 10 = 7
 			cin >> num;
-			queue[num/10][usedSize[num/10]] = num;
-			usedSize[num/10]++;
+			queue[num%10][usedSize[num%10]] = num;
+			usedSize[num%10]++;
 		}
 
-		int queue2[10][n];
-		int usedSize2[10] = {};
+		int queue2[10][n+3];
+		int usedSize2[10] = {0};
 		for(int i = 0; i < 10; i++){
 			for(int j = 0; j < usedSize[i]; j++){
-				queue2[ queue[i][]%10 ]
+				//put queue[i][j] into queue2
+				//87 / 10 = 8
+				queue2[ queue[i][j]/10 ][usedSize2[ queue[i][j]/10 ]] = queue[i][j];
+				usedSize2[ queue[i][j]/10 ]++;
 			}
 		}
+
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < usedSize2[i]; j++){
+				cout << queue2[i][j];
+				if(j != usedSize2[i]-1 || i != 9)
+					cout << " ";
+			}
+		}
+		cout << endl;
 		
 	}
 }
